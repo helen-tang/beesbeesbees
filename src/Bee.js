ed08221771ce41de749faff68905e83146b3707f
Helen Tang <helentang.93@gmail.com>
2017-03-27 11:55:24 -0700

    Implemented Bee class functionality

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+
+	Grub.call(this);
+	this.age = 5;
+	this.color = 'yellow';
+	this.job = 'keep on growing';
+
 };
 
+Bee.prototype = Object.create(Grub.prototype)
+// Bee.prototype = new Grub --> causes a lot of side effects (would override the properties?)
+
+Bee.prototype.constructor = Bee;
